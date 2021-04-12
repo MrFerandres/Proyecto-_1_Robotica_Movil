7a3b118e6c0d981c0f70896b61c995ec64eaf8b5
MrFerandres <61289385+MrFerandres@users.noreply.github.com>
2021-04-12 14:55:13 -0500

    Update Proyecto_1.py
    Agregamos comentarios

--- Proyecto_1.py
+++ Proyecto_1.py
@@ -1,6 +1,7 @@
 """
     Proyecto 1: Control de un robot movil
-    Equipo:
+    Equipo:Fernando Andres Chavez Gavaldon
+    Marco Antonio Ramirez Perez
 
     Codigo modificado de Juan-Pablo Ramirez-Paredes <jpi.ramirez@ugto.mx>
 """
@@ -15,8 +16,7 @@ import math as m
 import sim as vrep  # access all the VREP elements
 import random as rnd
 
-
-
+#Fucnion que gira 10 grados a la derecha
 def girar():
     t = 2.5/9
     tiempo = time.time() + 2.5/9
@@ -24,9 +24,6 @@ def girar():
         err = vrep.simxSetJointTargetVelocity(clientID, motorL, 1.0, vrep.simx_opmode_streaming)
         err = vrep.simxSetJointTargetVelocity(clientID, motorR, -1.0, vrep.simx_opmode_streaming)
     print("Salio de girar")
-
-
-
 
 
 def angdiff(t1, t2):
@@ -65,6 +62,7 @@ for i in range(16):
     err, state, point, detectedObj, detectedSurfNormVec = vrep.simxReadProximitySensor(clientID, usensor[i],
                                                                                        vrep.simx_opmode_streaming)
 
+#Inicializacion del robot
 ret, carpos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_streaming)
 ret, carrot = vrep.simxGetObjectOrientation(clientID, robot, -1, vrep.simx_opmode_streaming)
 
@@ -73,42 +71,54 @@ Kv = 0.5
 Kh = 2.5
 
 # xd and yd are the coordinates of the desired setpoint
-xd = np.zeros(2) #si se modifica este 10 se tiene que modificar el de yd y el del siguiente for
-yd = np.zeros(2)
+xd = np.zeros(11) #si se modifica este 10 se tiene que modificar el de yd y el del siguiente for
+yd = np.zeros(11)
+lugarx =[]
+lugary =[]
 
-for i in range(1):
-    xd[i]=4
-    yd[i]=0
-    #xd[i]=(rnd.uniform(-5.5, 5.5))
-    #yd[i]=(rnd.uniform(-5.5, 5.5))
-xd[1]=4
-yd[1]=5
+#Inicializacion de los puntos aleatorios
+for i in range(11):
+    xd[i]=(rnd.uniform(-5.5, 5.5))
+    yd[i]=(rnd.uniform(-5.5, 5.5))
+xd[0]=0
+yd[0]=0
 
 print(xd,yd)
 
+#variables de motores
 hd = 0
 r = 0.5 * 0.195
 L = 0.311
 
-
+#Ciclo de seguimiento de los puntos
 for i in range(len(xd)):
     errp = 10
-    while errp > 0.4:
+    watchdog =0
+    while errp > 0.2:
+        if watchdog>30:
+            print('Se salto el punto numero {}'.format(i+1))
+            break
+
+        #lugar acutal del robot
+        err, car_pos = vrep.simxGetObjectPosition(clientID, robot, -1, vrep.simx_opmode_oneshot_wait)
+        lugarx.append(car_pos[0])
+        lugary.append(car_pos[1])
 
 
-        for j in [2,3,4,5,6,7]:
+        for j in [1,2,3,4,5,6]: #Sensores que nos interesan que detecten algo [Sensores del 1-7]
             #print("Entro a sensores \n -------------",j)
             err, state, point, detectedObj, detectedSurfNormVec = vrep.simxReadProximitySensor(clientID, usensor[j],
                                                                                                vrep.simx_opmode_buffer)
             #print(state)
             if state:
-                print("se encontro con un muro")
+                print("se encontro con un muro en el sensor ",j)
                 break
         if state:
-            for alpha in range(5):
+            for alpha in range(5): #Apagamos motores
                 errf = vrep.simxSetJointTargetVelocity(clientID, motorL, 0, vrep.simx_opmode_streaming)
                 errf = vrep.simxSetJointTargetVelocity(clientID, motorR, 0, vrep.simx_opmode_streaming)
-            print("Se procede a girar 90 grados")
+            print('Se procede a girar 10 grados y sumamos al watchod ({}) '.format(watchdog))
+            watchdog=watchdog+1
             girar()
             continue
 
@@ -134,14 +144,18 @@ for i in range(len(xd)):
         v = Kv * errp
         omega = Kh * errh
 
-        ul = (v / r - L * omega / (2 * r))*0.5 # se multiplica por 0.5 porque la velocidad es mucha y cuando calcula otra vez el ERRP se pasa haciendo que este en un ciclo infinito
-        ur = (v / r + L * omega / (2 * r))*0.5
+        ul = (v / r - L * omega / (2 * r))*0.4 # se multiplica por 0.5 porque la velocidad es mucha y cuando calcula otra vez el ERRP se pasa haciendo que este en un ciclo infinito
+        ur = (v / r + L * omega / (2 * r))*0.4
         errf = vrep.simxSetJointTargetVelocity(clientID, motorL, ul, vrep.simx_opmode_streaming)
         errf = vrep.simxSetJointTargetVelocity(clientID, motorR, ur, vrep.simx_opmode_streaming)
         # time.sleep(0.1)
-    print('Punto {} encontrado \najajajajajajjaja\n'.format(i+1))
+    if watchdog>30: #decir si encontro punto o fue por el watchdog
+        print('El punto {} no se logro salir \nwatchdog\n'.format(i+1))
+    else:
+        print('Punto {} encontrado \n----------\n'.format(i+1))
 
-for i in range(10):
+for i in range(10): #apagar motores
+
     errf = vrep.simxSetJointTargetVelocity(clientID, motorL, 0, vrep.simx_opmode_streaming)
     errf = vrep.simxSetJointTargetVelocity(clientID, motorR, 0, vrep.simx_opmode_streaming)
     # time.sleep(0.1)
@@ -164,7 +178,7 @@ xnew = spi.splev(tnew, xc, der=0)
 ynew = spi.splev(tnew, yc, der=0)
 plt.plot(xnew, ynew)
 plt.plot(xd, yd, '.')
+plt.plot(lugarx,lugary,'r--')
 plt.show()
 
-
-print(xd,yd)
+#print(xd,yd)
